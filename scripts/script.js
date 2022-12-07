1a41da9d532c0637464ee13c9e428848440fb69f
DonnaBaijens <116149324+DonnaBaijens@users.noreply.github.com>
2022-12-07 16:25:28 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+const buttonNav = document.querySelector("header button");
+const menuNav = document.querySelector("header nav:nth-of-type(2)")
+
+buttonNav.addEventListener("click", openMenu);
+
+function openMenu() {
+    menuNav.classList.toggle("open");
+    buttonNav.classList.toggle("openMenu");
+    // menuNav.removeAttribute("inert");
+}
+
